538e4f5a81571c4fb544473857a827b47561960a
yasahaverland <yasahaverland@icloud.com>
2022-11-19 15:46:02 -0500

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'>
+                <div className='gpt3__navbar-links_logo'>
+                    <img src={logo} alt='logo'></img>
+                </div>    
+                <div className='gpt3__navbar-links_container'>
+                    <p>
+                        <a href='#hone'>Home</a>
+                    </p>
+                    <p>
+                        <a href='#wgpt3'>What is GPT3?</a>
+                    </p>
+                    <p>
+                        <a href='#possibility'>Open AI</a>
+                    </p>
+                    <p>
+                        <a href='#features'>Case Studies</a>
+                    </p>
+                    <p>
+                        <a href='#blog'>Library</a>
+                    </p>
+                </div>
+                <div className='gpt3__navbar-sign'>
+                    <p>Sign in</p>
+                </div>
+            </div>
         </div>
     )
 }
